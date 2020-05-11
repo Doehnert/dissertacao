a7e23b7645c9a3c011f1f97526266e1feeb6245b
eltonfd@gmail.com <eltonfd@gmail.com>
2020-05-11 15:15:36 -0300

    Arrumado capitulos

---
+++ suavizacao_malhas/no.py
@@ -0,0 +1,12 @@
+from numpy import *
+
+class No:
+    def __init__(self, nome, valor):
+        self.nome = nome
+        self.valor = asarray(valor)
+        self.fronteira = False
+
+        self.vizinhos = []
+
+        self.xl = 0
+        self.yl = 0
